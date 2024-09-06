dbddad425774509bd58c0dc0906c8f66d2ba849d
SambhavJain26 <worksambhav26@gmail.com>
2024-09-07 04:15:43 +0530

    node learning

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const fs = require('fs');
+const express = require('express');
+const app = express()
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+app.use(function(req, res, next){
+  next();
+}) 
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+app.listen(3000)
